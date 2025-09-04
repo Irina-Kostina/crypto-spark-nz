64f776b20ed1f0548cfaa8b3f7bbb66692cb15c5
Irina Kostina <irina.kostina.nz@gmail.com>
2025-09-04 18:58:16 +1200

    Fixed vite issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/crypto-spark-nz/',
+  // base: '/crypto-spark-nz/',
   server: {
     host: "::",
     port: 8080,
